8d26f972310c0848c53b67d0574b4326dcaf7308
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-22 16:46:35 +0100

    [add] style

--- javascript/script.js
+++ javascript/script.js
@@ -3,19 +3,21 @@ const container = document.querySelector('.container')
 for (let i=1; i<=100; i++){
   console.log(i);
   
-
+  const fizzbuzz = 'FizzBuzz';
+  const fizz = 'Fizz';
+  const buzz = 'Buzz';
 
   if ((!(i % 3)) && (!(i % 5))){
     container.innerHTML +=`
-    <div class="square threefive">${'FizzBuzz'}</div>
+    <div class="square threefive">${fizzbuzz}</div>
     `
   } else if (!(i % 3)){
     container.innerHTML +=`
-    <div class="square three">${'Fizz'}</div>
+    <div class="square three">${fizz}</div>
     `
   } else if (!(i % 5)){
     container.innerHTML += `
-    <div class="square five">${'Buzz'}</div>
+    <div class="square five">${buzz}</div>
     `
   } else{
     container.innerHTML += `
