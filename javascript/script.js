2cdc64e5436b0c43fe644c016e559ea87ea4fb4b
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-22 15:36:17 +0100

    [add] square with their class

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container')
+
+for (let i=1; i<=100; i++){
+  console.log(i);
+  
+  if (!(i % 3)){
+    container.innerHTML +=`
+    <div class="square three">${'Fizz'}</div>
+    `
+  } else if (!(i % 5)){
+    container.innerHTML += `
+    <div class="square five">${'Buzz'}</div>
+    `
+  } else{
+    container.innerHTML += `
+    <div class="square">${i}</div>
+    `
+  }
+
+}
